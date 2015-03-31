dca5a12b386e17caa2d23c97362ef901b8adb964
Fengda HUANG <h@phodal.com>
2015-03-31 23:13:03 +0800

    use js libs in vendor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,22 @@
 
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
+        jquery: 'vendor/jquery.min',
         jquerySidr: 'vendor/jquery.sidr.min',
         touchwipe: 'vendor/jquery.touchwipe.min',
 
-        react: '../bower_components/react/react-with-addons',
-        "JSXTransformer": '../bower_components/react/JSXTransformer',
-        jsx: '../bower_components/jsx-requirejs-plugin/js/jsx',
-        'react.backbone': '../bower_components/react.backbone/react.backbone',
+        react: 'vendor/react-with-addons.min',
+        "JSXTransformer": 'vendor/JSXTransformer',
+        jsx: 'vendor/jsx',
+        'react.backbone': 'vendor/react.backbone',
 
-        backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/lodash/dist/lodash',
+        backbone: 'vendor/backbone',
+        underscore: 'vendor/lodash.min',
 
-        text: '../bower_components/requirejs-text/text',
-        json: '../bower_components/requirejs-plugins/src/json',
-        mdown: '../bower_components/requirejs-plugins/src/mdown',
-        markdownConverter: '../bower_components/requirejs-plugins/lib/Markdown.Converter'
+        text: 'vendor/text',
+        json: 'vendor/json',
+        mdown: 'vendor/mdown',
+        markdownConverter: 'vendor/Markdown.Converter'
     },
     shim: {
         jquerySidr:['jquery'],
